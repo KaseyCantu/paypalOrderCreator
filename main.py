80d8e774923fad0c8479b6954a5b552968c110e0
Kasey Cantu <kpc.software.engineer@gmail.com>
2023-01-19 16:47:48 -0600

    Housekeeping :broom:

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from random import randint
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 user_email = input("Sandbox PayPal Account: ")
 user_password = input("Sandbox PayPal Password: ")
@@ -18,7 +15,7 @@ print('Options:', options)
 browser.implicitly_wait(6)
 
 
-def auto_browser():
+def auto_browser() -> None:
     idx = 0
     number_of_orders = int(number_of_orders_to_create)
 
@@ -36,7 +33,7 @@ def auto_browser():
         browser.quit()
 
 
-def paypal_login():
+def paypal_login() -> None:
     # Open Sandbox PayPal homepage in Google Chrome
     browser.get("https://www.sandbox.paypal.com/us/home")
 
@@ -48,62 +45,58 @@ def paypal_login():
     enter_user_password()
 
 
-def init_login():
+def init_login() -> None:
     homepage_login = browser.find_element(By.ID, 'ul-btn')  # Homepage | Login Button
     homepage_login.click()
 
 
-def enter_user_email():
+def enter_user_email() -> None:
     login_page_email_input = browser.find_element(By.ID, 'email')  # Login Page | Login Button
     login_page_email_input.clear()
     login_page_email_input.send_keys(user_email)
 
 
-def enter_user_password():
+def enter_user_password() -> None:
     browser.find_element(By.ID, 'btnNext').click()  # Login Page | Password Input
-    # sleep(1)
     login_page_password_input = browser.find_element(By.ID, 'password')  # Login Page | Password Input
     login_page_password_input.clear()
     login_page_password_input.send_keys(user_password)
     browser.find_element(By.ID, 'btnLogin').click()
 
 
-def navigate_to_send_money():
-    # sleep(4)
+def navigate_to_send_money() -> None:
     browser.find_element(By.XPATH, '//div[contains(text(), "Send money")]').click()  # Dashboard | Send money button
 
 
-def send_money():
+def send_money() -> None:
+    sleep(1)
     send_money_input = browser.find_element(By.XPATH, '//*[@id="fn-sendRecipient"]')  # Send Money Recipient | Input
     send_money_input.clear()
     send_money_input.send_keys(recipient)
-    # browser.find_element(By.XPATH, '//button[contains(text(), "Next")]').click()  # Send Money Recipient | Next button
     browser.find_element(By.XPATH, f'//div[contains(text(), "{recipient}")]').click()  # Send Money | Next button
 
 
-def enter_funding_amount():
-    # sleep(2)
+def enter_funding_amount() -> None:
     funding_input = browser.find_element(By.XPATH, '//*[@id="fn-amount"]')  # Funding Page | Amount input
     funding_input.send_keys(str(randint(100, 999)))
 
     browser.find_element(By.XPATH, '//button[contains(text(), "Continue")]').click()  # Funding Page | Continue button
 
 
-def send_payment_now():
+def send_payment_now() -> None:
     browser.find_element(By.XPATH, '//button[contains(text(), "Send Payment Now")]').click()
 
 
-def send_more_money():
+def send_more_money() -> None:
     browser.find_element(By.XPATH, '//a[contains(text(), "Send More Money")]').click()
 
 
-def send_money_to_create_orders():
+def send_money_to_create_orders() -> None:
     send_money()
     enter_funding_amount()
     send_payment_now()
     send_more_money()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     auto_browser()
