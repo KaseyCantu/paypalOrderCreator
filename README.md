9369ab3fa4b61230958ee3530aec6928e8f2bab1
Kasey Cantu <kpc.software.engineer@gmail.com>
2023-01-19 11:17:39 -0600

    To infinity and beyond! :rocket:

---
+++ README.md
@@ -0,0 +1,20 @@
+## Automated Selenium PayPal Order Generator
+
+This script runs on `Python 3` - if you do not already have it installed you can get it for [MacOS](https://www.python.org/downloads/macos/) or [Windows](https://www.python.org/downloads/windows/). You can verify which version you are currently running by using `python3 --version`.
+ 
+Alternatively, if you use `asdf` you can run `asdf install python 3.11.1` to get more easily download the latest stable version of `Python 3`.
+
+- Start by installing the script's dependencies by using the following command in your terminal from the project root:
+```bash
+pip3 install -r requirements.txt
+```
+
+- Run the script: 
+```bash
+python3 main.py
+```
+- You will be met with prompts that ask for your Sandbox Paypal Account username and password to be used for login. This will need to be the account you wish to send money from. 
+- Next you will be asked for the recipient PayPal account you wish to send money to.
+- Finally, you will be asked how many orders you would like to generate.
+
+> ATTN: :warning: Some websites do not run properly when running `headless` in Chrome. As a result, this script currently does not run headless. Coming soon will be an additional prompt when running the script that asks if you want to run `headless` and have `FireFox` installed to be able to do so.
---
+++ main.py
@@ -0,0 +1,109 @@
+from random import randint
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+user_email = input("Sandbox PayPal Account: ")
+user_password = input("Sandbox PayPal Password: ")
+recipient = input("Recipient Account: e.g. test.account+paypal99@auctane.com ")
+number_of_orders_to_create = input("How many orders do you want to create? ")
+
+browser = webdriver.Chrome()
+options = browser.create_options()
+print('Options:', options)
+browser.implicitly_wait(6)
+
+
+def auto_browser():
+    idx = 0
+    number_of_orders = int(number_of_orders_to_create)
+
+    try:
+        paypal_login()
+        navigate_to_send_money()
+
+        while idx < number_of_orders:
+            send_money_to_create_orders()
+            idx += 1
+            if idx == number_of_orders:
+                break
+
+    finally:
+        browser.quit()
+
+
+def paypal_login():
+    # Open Sandbox PayPal homepage in Google Chrome
+    browser.get("https://www.sandbox.paypal.com/us/home")
+
+    # Homepage | Click login button
+    init_login()
+
+    # Login Page | Fill out user login form
+    enter_user_email()
+    enter_user_password()
+
+
+def init_login():
+    homepage_login = browser.find_element(By.ID, 'ul-btn')  # Homepage | Login Button
+    homepage_login.click()
+
+
+def enter_user_email():
+    login_page_email_input = browser.find_element(By.ID, 'email')  # Login Page | Login Button
+    login_page_email_input.clear()
+    login_page_email_input.send_keys(user_email)
+
+
+def enter_user_password():
+    browser.find_element(By.ID, 'btnNext').click()  # Login Page | Password Input
+    # sleep(1)
+    login_page_password_input = browser.find_element(By.ID, 'password')  # Login Page | Password Input
+    login_page_password_input.clear()
+    login_page_password_input.send_keys(user_password)
+    browser.find_element(By.ID, 'btnLogin').click()
+
+
+def navigate_to_send_money():
+    # sleep(4)
+    browser.find_element(By.XPATH, '//div[contains(text(), "Send money")]').click()  # Dashboard | Send money button
+
+
+def send_money():
+    send_money_input = browser.find_element(By.XPATH, '//*[@id="fn-sendRecipient"]')  # Send Money Recipient | Input
+    send_money_input.clear()
+    send_money_input.send_keys(recipient)
+    # browser.find_element(By.XPATH, '//button[contains(text(), "Next")]').click()  # Send Money Recipient | Next button
+    browser.find_element(By.XPATH, f'//div[contains(text(), "{recipient}")]').click()  # Send Money | Next button
+
+
+def enter_funding_amount():
+    # sleep(2)
+    funding_input = browser.find_element(By.XPATH, '//*[@id="fn-amount"]')  # Funding Page | Amount input
+    funding_input.send_keys(str(randint(100, 999)))
+
+    browser.find_element(By.XPATH, '//button[contains(text(), "Continue")]').click()  # Funding Page | Continue button
+
+
+def send_payment_now():
+    browser.find_element(By.XPATH, '//button[contains(text(), "Send Payment Now")]').click()
+
+
+def send_more_money():
+    browser.find_element(By.XPATH, '//a[contains(text(), "Send More Money")]').click()
+
+
+def send_money_to_create_orders():
+    send_money()
+    enter_funding_amount()
+    send_payment_now()
+    send_more_money()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    auto_browser()
